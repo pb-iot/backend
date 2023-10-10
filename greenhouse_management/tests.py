88d6454e7bcce0a3b2301236a2652210de4e4622
Mateusz Więckowski <proyazz123@gmail.com>
2023-10-10 17:21:34 +0200

    feat: added model localization and a test

--- greenhouse_management/tests.py
+++ greenhouse_management/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Localization
 
-# Create your tests here.
+class LocalizationTestCase(TestCase):
+
+    def create_localization(self, name="Bialystok"):
+        return Localization.objects.create(name=name)
+    
+    def test_localization_creation(self):
+        obj = self.create_localization()
+        self.assertTrue(isinstance(obj, Localization))
+        self.assertTrue(Localization.objects.filter(name="Bialystok").exists())
+    
+
