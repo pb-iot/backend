d2bdeebd0d89101d3562cece8d98f5cbd0728bcf
Mateusz Więckowski <proyazz123@gmail.com>
2023-10-10 18:00:57 +0200

    feat: added user model and its tests

--- backend/settings.py
+++ backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'greenhouse_management',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -81,6 +82,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'user.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
