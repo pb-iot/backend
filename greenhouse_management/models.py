748a60e798b2548be900b3b9756ca57f4af7b47b
KamilSzczerbak <127205348+KamiloSzcz@users.noreply.github.com>
2023-10-25 12:53:51 +0200

    Feature/#12 Added device model (#44)
    * Added device model
    
    * added migrate

--- greenhouse_management/models.py
+++ greenhouse_management/models.py
@@ -8,15 +8,15 @@ from .managers import CustomUserManager
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_('email address'), unique=True, blank=False)
+    email = models.EmailField(_("email address"), unique=True, blank=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     objects = CustomUserManager()
 
@@ -30,10 +30,9 @@ class Location(models.Model):
 
 
 class GreenHouse(models.Model):
-
     class CropTypes(models.TextChoices):
-        TOMATOES = 'TT', _('Tomatoes')
-        POTATOES = 'PT', _('Potatoes')
+        TOMATOES = "TT", _("Tomatoes")
+        POTATOES = "PT", _("Potatoes")
 
     name = models.CharField(max_length=255)
     crop_type = models.CharField(
@@ -44,15 +43,29 @@ class GreenHouse(models.Model):
     # environment = models.ForeignKey(Environment,
     #                                 on_delete=models.CASCADE) <-- to consider
     #                                 )
-    location = models.ForeignKey(Location,
-                                 on_delete=models.CASCADE  # to consider
-                                 )
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)  # to consider
     # devices = models.ManyToManyField(Device)
     authorized_users = models.ManyToManyField(CustomUser)
-    owner = models.ForeignKey(CustomUser,
-                              on_delete=models.CASCADE,  # to consider
-                              related_name="owned_greenhouses")
+    owner = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,  # to consider
+        related_name="owned_greenhouses",
+    )
     # default_environment = models.ForeignKey(Environment,
     # on_delete=models.CASCADE, <-- to consider
     # related_name="default_greenhouses")
     # environments = models.ManyToManyField(Environment, related_name="greenhouses")
+
+
+class Device(models.Model):
+    class Functionality(models.TextChoices):
+        PASSIVE = "PA", _("Passive device")
+        ACTIVE = "AC", _("Active device")
+
+    name = models.CharField(max_length=100)
+    functionality = models.CharField(
+        max_length=2,
+        choices=Functionality.choices,
+        default=Functionality.ACTIVE,
+        blank=False,
+    )
