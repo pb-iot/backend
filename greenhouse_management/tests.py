455209bbf6ccfebf470daf69c1c91d56255fb691
KamilSzcz <kamilszczerbak43@gmail.com>
2023-10-25 12:35:15 +0200

    Added device model

--- greenhouse_management/tests.py
+++ greenhouse_management/tests.py
@@ -1,15 +1,13 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from .models import Location, GreenHouse
+from .models import *
 
 
 class UsersManagersTests(TestCase):
-
     def test_create_user(self):
         User = get_user_model()
-        user = User.objects.create_user(
-            email='normal@user.com', password='foo')
-        self.assertEqual(user.email, 'normal@user.com')
+        user = User.objects.create_user(email="normal@user.com", password="foo")
+        self.assertEqual(user.email, "normal@user.com")
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
@@ -22,15 +20,16 @@ class UsersManagersTests(TestCase):
         with self.assertRaises(TypeError):
             User.objects.create_user()
         with self.assertRaises(TypeError):
-            User.objects.create_user(email='')
+            User.objects.create_user(email="")
         with self.assertRaises(ValueError):
-            User.objects.create_user(email='', password="foo")
+            User.objects.create_user(email="", password="foo")
 
     def test_create_superuser(self):
         User = get_user_model()
         admin_user = User.objects.create_superuser(
-            email='super@user.com', password='foo')
-        self.assertEqual(admin_user.email, 'super@user.com')
+            email="super@user.com", password="foo"
+        )
+        self.assertEqual(admin_user.email, "super@user.com")
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
@@ -42,13 +41,13 @@ class UsersManagersTests(TestCase):
             pass
         with self.assertRaises(ValueError):
             User.objects.create_superuser(
-                email='super@user.com', password='foo', is_superuser=False)
+                email="super@user.com", password="foo", is_superuser=False
+            )
 
-            filter(catergory=map(lambda key: 'europe' in key))
+            filter(catergory=map(lambda key: "europe" in key))
 
 
 class LocationTestCase(TestCase):
-
     def create_location(self, name="Bialystok", point=None):
         return Location.objects.create(name=name, coordinates=point)
 
@@ -57,23 +56,19 @@ class LocationTestCase(TestCase):
         created_obj = self.create_location(point=coords)
         created_obj.save()
         filtered_obj = Location.objects.get(id=created_obj.id)
-        
+
         self.assertTrue(isinstance(filtered_obj, Location))
         self.assertTrue(Location.objects.filter(coordinates=coords).exists())
 
 
 class GreenHouseTestCase(TestCase):
-
     def test_greenhouse_creation(self):
         name = "TomatoTomato"
-        crop_type = 'TT'
+        crop_type = "TT"
         User = get_user_model()
-        owner = User.objects.create_user(
-            email='owner@user.com', password='foo')
-        user_1 = User.objects.create_user(
-            email='normal1@user.com', password='foo')
-        user_2 = User.objects.create_user(
-            email='normal2@user.com', password='foo')
+        owner = User.objects.create_user(email="owner@user.com", password="foo")
+        user_1 = User.objects.create_user(email="normal1@user.com", password="foo")
+        user_2 = User.objects.create_user(email="normal2@user.com", password="foo")
         authorized_users = [user_1, user_2]
         location = Location.objects.create(name="Bialystok")
         # environment <-- add test
@@ -81,32 +76,44 @@ class GreenHouseTestCase(TestCase):
         # default_environment <-- add test
         # environments <-- add test
 
-        obj = GreenHouse.objects.create(name=name,
-                                        location=location,
-                                        owner=owner)
+        obj = GreenHouse.objects.create(name=name, location=location, owner=owner)
         obj.authorized_users.set(authorized_users)
 
         self.assertTrue(isinstance(obj, GreenHouse))
-        self.assertTrue(GreenHouse.objects.filter(name=name,
-                                                  crop_type=crop_type,
-                                                  location=location,
-                                                  authorized_users__in=[
-                                                      user_1.id, user_2.id],
-                                                  owner=owner).exists())
+        self.assertTrue(
+            GreenHouse.objects.filter(
+                name=name,
+                crop_type=crop_type,
+                location=location,
+                authorized_users__in=[user_1.id, user_2.id],
+                owner=owner,
+            ).exists()
+        )
 
     def test_crop_type_choices(self):
         # Test if crop_types contains one of available options
 
         # Case 1: option: 'Tomatoes' (TOMATOES)
-        greenhouse_tomatoes = GreenHouse(
-            crop_type=GreenHouse.CropTypes.TOMATOES)
-        self.assertEqual(greenhouse_tomatoes.crop_type, 'TT')
+        greenhouse_tomatoes = GreenHouse(crop_type=GreenHouse.CropTypes.TOMATOES)
+        self.assertEqual(greenhouse_tomatoes.crop_type, "TT")
 
         # Case 2: option: 'Potatoes' (POTATOES)
-        greenhouse_potatoes = GreenHouse(
-            crop_type=GreenHouse.CropTypes.POTATOES)
-        self.assertEqual(greenhouse_potatoes.crop_type, 'PT')
+        greenhouse_potatoes = GreenHouse(crop_type=GreenHouse.CropTypes.POTATOES)
+        self.assertEqual(greenhouse_potatoes.crop_type, "PT")
 
         # Case 3: option: default should be 'Tomatoes'
         default_greenhouse = GreenHouse()
-        self.assertEqual(default_greenhouse.crop_type, 'TT')
+        self.assertEqual(default_greenhouse.crop_type, "TT")
+
+
+class DeviceTestCase(TestCase):
+    def device_creation(self):
+        return Device.objects.create(name="Fan", functionality="AC")
+
+    def test_device_creation(self):
+        device = self.device_creation()
+        self.assertEqual(device.name, "Fan")
+        functionality_choices = dict(device.Functionality.choices)
+        self.assertIn(device.functionality, functionality_choices)
+        self.assertTrue(isinstance(device, Device))
+        self.assertTrue(Device.objects.filter(name="Fan").exists())
