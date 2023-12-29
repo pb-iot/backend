f735ed053b5cee812d00f898024813e905632a0f
Patrycja Rećko <patrycjarecko00@gmail.com>
2023-12-29 17:49:49 +0100

    feat: specified list of fields for sorting in admin page

--- greenhouse_management/admin.py
+++ greenhouse_management/admin.py
@@ -9,11 +9,17 @@ class DeviceAdmin(admin.ModelAdmin):
     list_filter = ('functionality',)
     search_fields = ("name",)
 
+    class Meta:
+        ordering = ('name', 'functionality')
+
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('name', 'coordinates')
     search_fields = ("name",)
+
+    class Meta:
+        ordering = ('name',)
 
 
 @admin.register(CustomUser)
@@ -22,12 +28,18 @@ class DeviceAdmin(admin.ModelAdmin):
     list_filter = ('date_joined', 'is_staff', 'is_active')
     search_fields = ('first_name', 'last_name', 'email')
 
+    class Meta:
+        ordering = ('first_name', 'last_name', 'email', 'date_joined', 'is_staff', 'is_active')
+
 
 @admin.register(GreenHouse)
 class DeviceAdmin(admin.ModelAdmin):
     list_display = ('name', 'crop_type', 'display_location', 'display_authorized_users', 'display_owner')
     list_filter = ('crop_type', 'location__name', 'owner')
     search_fields = ('name',)
+
+    class Meta:
+        ordering = ('name', 'crop_type', 'display_location', 'display_owner')
 
     def display_authorized_users(self, obj):
         return ', '.join(f'{user.first_name} {user.last_name}' for user in obj.authorized_users.all())
