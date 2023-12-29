67d2bf4209b68d6e0f67f8f9680fb86563874630
Patrycja Rećko <patrycjarecko00@gmail.com>
2023-12-29 17:40:41 +0100

    feat: specified list of fields for searching in admin page

--- greenhouse_management/admin.py
+++ greenhouse_management/admin.py
@@ -7,23 +7,27 @@ from greenhouse_management.models import *
 class DeviceAdmin(admin.ModelAdmin):
     list_display = ('name', 'functionality')
     list_filter = ('functionality',)
+    search_fields = ("name",)
 
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('name', 'coordinates')
+    search_fields = ("name",)
 
 
 @admin.register(CustomUser)
 class DeviceAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'date_joined', 'is_staff', 'is_active')
     list_filter = ('date_joined', 'is_staff', 'is_active')
+    search_fields = ('first_name', 'last_name', 'email')
 
 
 @admin.register(GreenHouse)
 class DeviceAdmin(admin.ModelAdmin):
     list_display = ('name', 'crop_type', 'display_location', 'display_authorized_users', 'display_owner')
     list_filter = ('crop_type', 'location__name', 'owner')
+    search_fields = ('name',)
 
     def display_authorized_users(self, obj):
         return ', '.join(f'{user.first_name} {user.last_name}' for user in obj.authorized_users.all())
